f8227dd8add1b91675b4fe22ad88428c38743090
Tyler Matthews <tyler.matthews@me.com>
2013-05-16 18:06:06 -0400

    add User show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Omrails::Application.routes.draw do
+  get "users/show"
+
   resources :pins
 
 
-  devise_for :users
+  devise_for :users 
+  match 'users/:id' => 'users#show', as: :user
 
-get 'about' => 'pages#about'
+  get 'about' => 'pages#about'
 
   root :to => 'pins#index'
 
